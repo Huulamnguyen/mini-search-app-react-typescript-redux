bf3579950b6af97e157576e4cd5d3f42e47e92b6
Huu Lam Nguyen <liamnguyen.swe@gmail.com>
2023-06-21 18:05:49 -0400

    Completed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,17 @@
+import { Provider } from "react-redux";
+import { store } from "../state";
+
+import RepositoriesList from "./RepositoriesList";
+
 const App = () => {
-  return <p>React Typescript App</p>;
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search For a Package:</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
 };
 
 export default App;
